d20ab6ed025064795c1bd168ae96a2904dcc8c15
Julien Az <54361213+JustFS@users.noreply.github.com>
2020-07-13 14:11:27 +0200

    finished

--- script.js
+++ script.js
@@ -1,57 +1,38 @@
 const main = document.querySelector(".main");
-let x = 0;
-let y = 0;
-let dragElement;
 
-const OnMouseDown = (e) => {
-  let target = e.target != null ? e.target : e.srcElement;
+document.addEventListener('mousedown', (e) => {
+  // set mousemove-X
+  if (e.target.className == "drag-x" && (window.event != null)) {
 
-  if ((window.event != null) && target.className == "drag-x") {
-    x = e.clientX;
-    dragElement = target;
-    document.onmousemove = OnMouseMoveX;
-    document.body.focus();
-
-    document.onselectstart = () => {return false};
-    target.ondragstart = () => {return false};
-    return false;
-  } 
-  else if ((window.event != null) && target.className == "drag-y") {
-    y = e.clientY;
-    dragElement = target;
-    document.onmousemove = OnMouseMoveY;
-    document.body.focus();
-
-    document.onselectstart = () => {return false};
-    target.ondragstart = () => {return false};
-    return false;
+    document.onmousemove = (e) => {
+      e == null ? e = window.event : '';
+      document.querySelector(".box-1").style.flex =
+        "0 0" + e.clientX / (main.clientWidth / 100) + "%";
+    }
+    preventBehavior(e);
   }
-}
+  // set mousemove-Y
+  else if (e.target.className == "drag-y" && (window.event != null)) {
 
-const OnMouseMoveX = (e) => {
-  e == null ? e = window.event : '';
-  document.querySelector(".box-1").style.flex =
-    "0 0" + e.clientX / (main.clientWidth / 100) + "%";
-}
-
-const OnMouseMoveY = (e) => {
-  if (e == null) var e = window.event;
-  document.querySelector(".top").style.flex =
-    "0 0" + e.clientY / (main.clientHeight / 100) + "%";
-}
-
-const OnMouseUp = () => {
-  if (dragElement != null) {
+    document.onmousemove = (e) => {
+      e == null ? e = window.event : '';
+      document.querySelector(".top").style.flex =
+        "0 0" + e.clientY / (main.clientHeight / 100) + "%";
+    }
+    preventBehavior(e);
+  }
+})
+// stop everthing on mouseup
+document.addEventListener('mouseup', (e) => {
+  if (e.target != null) {
     document.onmousemove = null;
     document.onselectstart = null;
-    dragElement.ondragstart = null;
-    dragElement = null;
+    e.target.ondragstart = null;
   }
+})
+// prevent bad behavior on the moving element
+const preventBehavior = (e) => {
+  document.onselectstart = () => {return false};
+  e.target.ondragstart = () => {return false};
+  return false;
 }
-
-const InitDragDrop = () => {
-  document.onmousedown = OnMouseDown;
-  document.onmouseup = OnMouseUp;
-}
-
-InitDragDrop();
